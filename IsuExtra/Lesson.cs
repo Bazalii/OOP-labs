ec4b8fa70cb772b9192fe0ea08464543cc2f3cfc
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-10-16 22:21:49 +0300

    feat: add Lesson class

---
+++ IsuExtra/Lesson.cs
@@ -0,0 +1,15 @@
+﻿namespace IsuExtra
+{
+    public class Lesson
+    {
+        public Lesson(string time, int room)
+        {
+            Time = time;
+            Room = room;
+        }
+
+        public string Time { get; }
+
+        public int Room { get; }
+    }
+}
