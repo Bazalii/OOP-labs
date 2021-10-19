ba4ca1318c0ae8744adf8a23c8a884b9f0be7ab9
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-10-19 16:46:12 +0300

    feat: add tests and new folder to project
    - Add 4 new tests
    - Add Tools folder to project model

--- IsuExtra.Tests/IsuExtraTest.cs
+++ IsuExtra.Tests/IsuExtraTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using IsuExtra.Services;
 using NUnit.Framework;
 
@@ -19,27 +20,70 @@ namespace IsuExtra.Tests
             JointTrainingGroupService.GetStudyGroup("M3204").AddLesson("Monday. 8:20-9:50", 240);
             JointTrainingGroupService.GetStudyGroup("M3204").AddLesson("Monday. 10:00-11:30", 466);
             JointTrainingGroupService.AddStudent(JointTrainingGroupService.GetStudyGroup("M3204"), "Ivan");
+            
+            JointTrainingGroupService.AddStudyGroup(JointTrainingGroupService.GetMegaFaculty("FTM"), "R3301");
+            JointTrainingGroupService.GetStudyGroup("R3301").AddLesson("Monday. 8:20-9:50", 240);
+            JointTrainingGroupService.GetStudyGroup("R3301").AddLesson("Thursday. 10:00-11:30", 310);
+            JointTrainingGroupService.AddStudent(JointTrainingGroupService.GetStudyGroup("R3301"), "Alexey");
 
             JointTrainingGroupService.AddTrainingGroup(JointTrainingGroupService.GetMegaFaculty("FITIP"), "Discrete Math");
             JointTrainingGroupService.GetTrainingGroup("Discrete Math").AddStream(new Stream("DM 1.1"));
             JointTrainingGroupService.GetTrainingGroup("Discrete Math").GetStream("DM 1.1").AddLesson("Thursday. 10:00-11:30", 238);
             JointTrainingGroupService.GetTrainingGroup("Discrete Math").GetStream("DM 1.1").AddLesson("Thursday. 15:20-16:50", 466);
-
+            JointTrainingGroupService.GetTrainingGroup("Discrete Math").AddStream(new Stream("DM 1.2"));
+            JointTrainingGroupService.GetTrainingGroup("Discrete Math").GetStream("DM 1.2").AddLesson("Monday. 8:20-9:50", 238);
+            JointTrainingGroupService.GetTrainingGroup("Discrete Math").GetStream("DM 1.2").AddLesson("Thursday. 15:20-16:50", 466);
+            
             JointTrainingGroupService.AddTrainingGroup(JointTrainingGroupService.GetMegaFaculty("FTM"), "Photonics");
             JointTrainingGroupService.GetTrainingGroup("Photonics").AddStream(new Stream("Ph 1.1"));
-            JointTrainingGroupService.GetTrainingGroup("Photonics").GetStream("Ph 1.1").AddLesson("Wednesday. 10:00-11:30", 100);
-            JointTrainingGroupService.GetTrainingGroup("Photonics").GetStream("Ph 1.1").AddLesson("Thursday. 15:20-16:50", 466);
+            JointTrainingGroupService.GetTrainingGroup("Photonics").GetStream("Ph 1.1").AddLesson("Monday. 10:00-11:30", 238);
+            JointTrainingGroupService.GetTrainingGroup("Photonics").GetStream("Ph 1.1").AddLesson("Wednesday. 15:20-16:50", 466);
             JointTrainingGroupService.GetTrainingGroup("Photonics").AddStream(new Stream("Ph 1.2"));
-            JointTrainingGroupService.GetTrainingGroup("Photonics").GetStream("Ph 1.2").AddLesson("Tuesday. 10:00-11:30", 105);
-            JointTrainingGroupService.GetTrainingGroup("Photonics").GetStream("Ph 1.2").AddLesson("Friday. 10:00-11:30", 329);
+            JointTrainingGroupService.GetTrainingGroup("Photonics").GetStream("Ph 1.2").AddLesson("Tuesday. 11:40-13:10", 238);
+            JointTrainingGroupService.GetTrainingGroup("Photonics").GetStream("Ph 1.2").AddLesson("Thursday. 15:20-16:50", 466);
         }
 
         [Test]
         public void Enroll_StudentEnrollsToJointTrainingGroup_JointTrainingGroupHasStudent()
         {
             JointTrainingGroupService.Enroll(JointTrainingGroupService.GetStudent(1), "Photonics");
-            Console.WriteLine(JointTrainingGroupService.GetTrainingGroup("Photonics").GetStream("Ph 1.1").Students[0]);
-            Assert.IsTrue(JointTrainingGroupService.GetTrainingGroup("Photonics").GetStream("Ph 1.1").GetStudent(1) != null);
+            Assert.IsTrue(JointTrainingGroupService.GetTrainingGroup("Photonics").GetStream("Ph 1.2").GetStudent(1) != null);
+        }
+        
+        [Test]
+        public void Enroll_StudentEnrollsToJointTrainingGroupFromHisFaculty_ThrowException()
+        {
+            Assert.Catch<StudentFacultyGroupException>(() =>
+            {
+                JointTrainingGroupService.Enroll(JointTrainingGroupService.GetStudent(1), "Discrete Math");
+            });
+        }
+        
+        [Test]
+        public void Enroll_StudentEnrollsToJointTrainingGroupWithCrossingLessons_ThrowException()
+        {
+            Assert.Catch<NoAvailableStreamsException>(() =>
+            {
+                JointTrainingGroupService.Enroll(JointTrainingGroupService.GetStudent(2), "Discrete Math");
+            });
+        }
+        
+        [Test]
+        public void CancelEntry_StudentCancelsEntryToJointTrainingGroup_StudentIsNotInJointTrainingGroup()
+        {
+            JointTrainingGroupService.CancelEntry(JointTrainingGroupService.GetStudent(1), "Photonics");
+            Assert.IsTrue(JointTrainingGroupService.GetTrainingGroup("Photonics").GetStream("Ph 1.2").GetStudent(1) == null);
+        }
+        
+        [Test]
+        public void GetStreams_GetAllJointTrainingGroupStreams_ReceiveAllCorrespondingStreams()
+        {
+            IReadOnlyList<Stream> wantedStreams = JointTrainingGroupService.GetStreams("Photonics");
+            foreach (Stream stream in wantedStreams)
+            {
+                Console.WriteLine(stream.Name);
+            }
+            Assert.IsTrue(wantedStreams.Count == 2 && wantedStreams[0].Name == "Ph 1.1" && wantedStreams[1].Name == "Ph 1.2");
         }
     }
 }
