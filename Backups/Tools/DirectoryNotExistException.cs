3eb9db05cba882c5e0542d3b936e2da5897b7eb2
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-11-19 23:41:51 +0300

    feat: add new exception

---
+++ Backups/Tools/DirectoryNotExistException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Backups.Tools
+{
+    public class DirectoryNotExistException : Exception
+    {
+        public DirectoryNotExistException(string message)
+            : base(message)
+        {
+        }
+    }
+}
