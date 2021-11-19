c77b4a47351cf9280b65f3a5014730871fce3699
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-11-20 00:23:02 +0300

    feat: some improvements in BackupJob
    - add two new fields
    - correct Process() method

--- Backups/BackupStructure/BackupJob.cs
+++ Backups/BackupStructure/BackupJob.cs
@@ -6,23 +6,28 @@ namespace Backups.BackupStructure
 {
     public class BackupJob
     {
+        private int _restorePointsCounter;
+
+        private string _restorePointName = "RestorePoint";
+
         private List<JobObject> _jobObjects = new ();
 
         private List<RestorePoint> _restorePoints = new ();
 
-        public BackupJob(ISavingAlgorithm savingAlgorithm)
+        public BackupJob(SavingAlgorithm savingAlgorithm)
         {
             SavingAlgorithm = savingAlgorithm ??
                               throw new ArgumentNullException(
                                   nameof(savingAlgorithm), "SavingAlgorithm cannot be null!");
         }
 
-        public ISavingAlgorithm SavingAlgorithm { get; set; }
+        public SavingAlgorithm SavingAlgorithm { get; set; }
 
         public void Process()
         {
-            SavingAlgorithm.Backup(_jobObjects);
+            SavingAlgorithm.Backup(_jobObjects, _restorePointName + _restorePointsCounter);
             _restorePoints.Add(new RestorePoint(DateTime.Now, _jobObjects));
+            _restorePointsCounter += 1;
         }
 
         public void AddJobObject(JobObject jobObject)
