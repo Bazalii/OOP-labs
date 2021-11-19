6f2af94df9fffcfde23ba4d0c8b4f3ee114a2596
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-11-19 19:04:14 +0300

    feat: realise all methods for work with Windows FS

--- Backups/FileSystem/Implementations/WindowsFileSystem.cs
+++ Backups/FileSystem/Implementations/WindowsFileSystem.cs
@@ -1,6 +1,47 @@
-﻿namespace Backups.FileSystem.Implementations
+﻿using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace Backups.FileSystem.Implementations
 {
     public class WindowsFileSystem : IFileSystem
     {
+        public void WriteToFile(string pathToFile, string textToWrite)
+        {
+            FileStream file = File.Create(pathToFile);
+            byte[] information = new UTF8Encoding(true).GetBytes(textToWrite);
+            file.Write(information, 0, information.Length);
+            file.Close();
+        }
+
+        public void AddToArchive(string directoryToArchivePath, string archivePath)
+        {
+            ZipFile.CreateFromDirectory(directoryToArchivePath, archivePath);
+        }
+
+        public void CreateDirectory(string pathToNewDirectory)
+        {
+            Directory.CreateDirectory(pathToNewDirectory);
+        }
+
+        public void RemoveDirectory(string pathToDirectory)
+        {
+            Directory.Delete(pathToDirectory);
+        }
+
+        public void CreateFile(string pathToFile)
+        {
+            File.Create(pathToFile).Close();
+        }
+
+        public void CopyFile(string oldPath, string newPath)
+        {
+            File.Copy(oldPath, newPath);
+        }
+
+        public void RemoveFile(string pathToFile)
+        {
+            File.Delete(pathToFile);
+        }
     }
 }
