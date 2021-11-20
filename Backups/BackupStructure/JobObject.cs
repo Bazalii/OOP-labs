2d3d74df59e5cd497b28be44f447eac91c753f4f
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-11-20 23:07:35 +0300

    feat: add Equals methods to JobObject class
    - Now JobObject can be conveniently removed from List<JobObject>

--- Backups/BackupStructure/JobObject.cs
+++ Backups/BackupStructure/JobObject.cs
@@ -11,6 +11,21 @@ namespace Backups.BackupStructure
                              nameof(pathToFile), "PathToFile cannot be null!");
         }
 
-        public string PathToFile { get; set; }
+        public string PathToFile { get; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is JobObject jobObject && Equals(jobObject);
+        }
+
+        public override int GetHashCode()
+        {
+            return PathToFile != null ? PathToFile.GetHashCode() : 0;
+        }
+
+        private bool Equals(JobObject other)
+        {
+            return PathToFile == other.PathToFile;
+        }
     }
 }
