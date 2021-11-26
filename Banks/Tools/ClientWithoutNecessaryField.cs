4b6d8fc2053e6d88de3855f4dc070a894e45eb9c
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-11-26 23:08:10 +0300

    feat: add new exception

---
+++ Banks/Tools/ClientWithoutNecessaryField.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Banks.Tools
+{
+    public class ClientWithoutNecessaryField : Exception
+    {
+        public ClientWithoutNecessaryField(string message)
+            : base(message)
+        {
+        }
+    }
+}
