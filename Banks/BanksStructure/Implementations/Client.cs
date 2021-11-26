8d8816aa8e9d0a7b5d0a3f3bfcafc464eabd2811
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-11-26 23:09:04 +0300

    feat: realise Client class and add its builder

--- Banks/BanksStructure/Implementations/Client.cs
+++ Banks/BanksStructure/Implementations/Client.cs
@@ -2,5 +2,56 @@
 {
     public class Client
     {
+        private string _name;
+
+        private string _surname;
+
+        private string _address;
+
+        private int _passportNumber;
+
+        internal Client()
+        {
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public string GetSurname()
+        {
+            return _surname;
+        }
+
+        public void SetAddress(string address)
+        {
+            _address = address;
+        }
+
+        public string GetAddress()
+        {
+            return _address;
+        }
+
+        public void SetPassportNumber(int passportNumber)
+        {
+            _passportNumber = passportNumber;
+        }
+
+        public int GetPassportNumber()
+        {
+            return _passportNumber;
+        }
+
+        internal void SetName(string name)
+        {
+            _name = name;
+        }
+
+        internal void SetSurname(string surname)
+        {
+            _surname = surname;
+        }
     }
 }
