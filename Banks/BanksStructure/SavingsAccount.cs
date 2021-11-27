bb5bf283a7c8d97795b126985618f6596c16665f
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-11-27 23:05:04 +0300

    feat: add abstract class for savings accounts

---
+++ Banks/BanksStructure/SavingsAccount.cs
@@ -0,0 +1,32 @@
+﻿namespace Banks.BanksStructure
+{
+    public abstract class SavingsAccount : Account
+    {
+        protected SavingsAccount(int id, int term, int percent, float amountOfMoney, bool doubtfulness, float limitIfIsDoubtful)
+        {
+            Id = id;
+            Term = term;
+            Percent = percent;
+            AmountOfMoney = amountOfMoney;
+            IsDoubtful = doubtfulness;
+            LimitIfIsDoubtful = limitIfIsDoubtful;
+            DailyPercent = Percent * AmountOfMoney / 365;
+        }
+
+        protected float MonthlyIncome { get; set; }
+
+        protected float Percent { get; set; }
+
+        protected float DailyPercent { get; set; }
+
+        public override void AddDailyIncome()
+        {
+            MonthlyIncome += DailyPercent;
+        }
+
+        public void AddMonthlyIncome()
+        {
+            AmountOfMoney += MonthlyIncome;
+        }
+    }
+}
