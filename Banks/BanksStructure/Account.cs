a125300505d8499eddb5a313828ef43a90e1a4e2
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-11-27 23:05:45 +0300

    feat: realise Account abstract class

--- Banks/BanksStructure/Account.cs
+++ Banks/BanksStructure/Account.cs
@@ -2,5 +2,59 @@
 {
     public abstract class Account
     {
+        protected int Id { get; init; }
+
+        protected int Term { get; init; }
+
+        protected int DaysLeft { get; set; }
+
+        protected float AmountOfMoney { get; set; }
+
+        protected bool IsDoubtful { get; set; }
+
+        protected float LimitIfIsDoubtful { get; set; }
+
+        public abstract void AddDailyIncome();
+
+        public abstract void WithdrawMoney(float amountOfMoney);
+        public void AddMoney(float amountOfMoney)
+        {
+            AmountOfMoney += amountOfMoney;
+        }
+
+        public int GetId()
+        {
+            return Id;
+        }
+
+        public int GetTerm()
+        {
+            return Term;
+        }
+
+        public int GetDaysLeft()
+        {
+            return DaysLeft;
+        }
+
+        public int GetTermAndDaysLeftDiff()
+        {
+            return Term - DaysLeft;
+        }
+
+        public float GetAmountOfMoney()
+        {
+            return AmountOfMoney;
+        }
+
+        public void SetDoubtfulness(bool doubtfulness)
+        {
+            IsDoubtful = doubtfulness;
+        }
+
+        public bool GetDoubtfulness()
+        {
+            return IsDoubtful;
+        }
     }
 }
