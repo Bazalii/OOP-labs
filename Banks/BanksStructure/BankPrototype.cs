4b22954cbbb849913dc12f9593d5e54366d2a92e
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-11-27 23:03:49 +0300

    feat: realise Bank and BankPrototype

--- Banks/BanksStructure/BankPrototype.cs
+++ Banks/BanksStructure/BankPrototype.cs
@@ -1,6 +1,20 @@
-﻿namespace Banks.BanksStructure
+﻿using System.Collections.Generic;
+using Banks.BanksStructure.Implementations;
+
+namespace Banks.BanksStructure
 {
     public abstract class BankPrototype
     {
+        protected List<Account> Accounts { get; set; } = new ();
+
+        protected List<Client> Clients { get; set; } = new ();
+
+        public abstract void AddAccount(Account account);
+
+        public abstract void AddClient(Client client);
+
+        public abstract bool CheckIfMonthPassed(Account account);
+
+        public abstract void AddDailyIncome();
     }
 }
--- Banks/BanksStructure/Implementations/Bank.cs
+++ Banks/BanksStructure/Implementations/Bank.cs
@@ -1,6 +1,34 @@
-﻿namespace Banks.BanksStructure.Implementations
+﻿using System.Collections.Generic;
+
+namespace Banks.BanksStructure.Implementations
 {
-    public class Bank
+    public class Bank : BankPrototype
     {
+        public override void AddAccount(Account account)
+        {
+            Accounts.Add(account);
+        }
+
+        public override void AddClient(Client client)
+        {
+            Clients.Add(client);
+        }
+
+        public override bool CheckIfMonthPassed(Account account)
+        {
+            return account.GetTermAndDaysLeftDiff() % 30 == 0;
+        }
+
+        public override void AddDailyIncome()
+        {
+            foreach (Account account in Accounts)
+            {
+                account.AddDailyIncome();
+                if (account is SavingsAccount savingsAccount && CheckIfMonthPassed(account))
+                {
+                    savingsAccount.AddMonthlyIncome();
+                }
+            }
+        }
     }
 }
