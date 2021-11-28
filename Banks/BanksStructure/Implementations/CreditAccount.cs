80cb558673b5a6722c246491da4ae22ad9958ef0
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-11-28 20:38:45 +0300

    feat: improve CreditAccount class
    - Add constructor
    - Add realisation of ReduceDaysLeft method

--- Banks/BanksStructure/Implementations/CreditAccount.cs
+++ Banks/BanksStructure/Implementations/CreditAccount.cs
@@ -2,19 +2,38 @@
 {
     public class CreditAccount : Account
     {
-        private int Commission { get; set; }
+        private float _commission;
+
+        public CreditAccount(int id, int term, float commission, float amountOfMoney, bool doubtfulness, float limitIfIsDoubtful)
+        {
+            Id = id;
+            Term = term;
+            _commission = commission;
+            AmountOfMoney = amountOfMoney;
+            IsDoubtful = doubtfulness;
+            LimitIfIsDoubtful = limitIfIsDoubtful;
+        }
 
         public override void AddDailyIncome()
         {
             if (AmountOfMoney < 0)
             {
-                AmountOfMoney -= Commission;
+                AmountOfMoney -= _commission;
             }
         }
 
         public override void WithdrawMoney(float amountOfMoney)
         {
             AmountOfMoney -= amountOfMoney;
+        }
+
+        public override void ReduceDaysLeft()
+        {
+            DaysLeft -= 1;
+            if (DaysLeft == 0)
+            {
+                DaysLeft += 180;
+            }
         }
     }
 }
