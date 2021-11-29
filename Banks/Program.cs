cb94ae722ec4bd4094b4ec71382cb8a3f4661b48
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-11-30 00:41:40 +0300

    feat: add starting of an application to Program.cs

--- Banks/Program.cs
+++ Banks/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Banks
+﻿using Banks.ConsoleInterfaceStructure;
+
+namespace Banks
 {
     internal static class Program
     {
         private static void Main()
         {
+            Application application = new ();
+            application.Process();
         }
     }
 }
