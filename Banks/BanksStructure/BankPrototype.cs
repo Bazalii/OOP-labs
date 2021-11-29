6e4be4e46db626b33c8083435d709c17ec94fcde
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-11-29 23:49:17 +0300

    feat: improve Bank classes
    - Add Equals methods
    - Add several logic methods
    - Change generation of Accounts Ids

--- Banks/BanksStructure/BankPrototype.cs
+++ Banks/BanksStructure/BankPrototype.cs
@@ -5,6 +5,10 @@ namespace Banks.BanksStructure
 {
     public abstract class BankPrototype
     {
+        public IReadOnlyList<Account> ReadableAccounts => Accounts;
+
+        protected int Id { get; set; }
+
         protected string Name { get; set; }
 
         protected int AccountIds { get; set; }
@@ -21,13 +25,21 @@ namespace Banks.BanksStructure
 
         public abstract void AddAccount(Account account);
 
+        public abstract void RemoveAccount(Account account);
+
+        public abstract void CloseAccount(Account account);
+
         public abstract void CreateDepositAccount(Client client, float amountOfMoney);
+
+        public abstract void CloseDepositAccount(Account account);
 
         public abstract void CreateDebitAccount(Client client, float amountOfMoney);
 
         public abstract void CreateCreditAccount(Client client, float amountOfMoney);
 
         public abstract void AddClient(Client client);
+
+        public abstract Client GetClientByAccountId(string accountId);
 
         public abstract bool GetClientDoubtfulness(Client client);
 
@@ -36,6 +48,16 @@ namespace Banks.BanksStructure
         public abstract void AddDailyIncome();
 
         public abstract void ReduceDaysLeft();
+
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+
+        public int GetId()
+        {
+            return Id;
+        }
 
         public string GetName()
         {
--- Banks/BanksStructure/Implementations/Bank.cs
+++ Banks/BanksStructure/Implementations/Bank.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
 
 namespace Banks.BanksStructure.Implementations
 {
@@ -17,40 +17,60 @@ namespace Banks.BanksStructure.Implementations
             Accounts.Add(account);
         }
 
+        public override void RemoveAccount(Account account)
+        {
+            GetClientByAccountId(account.GetId()).RemoveAccount(account);
+            Accounts.Remove(account);
+        }
+
+        public override void CloseAccount(Account account)
+        {
+            RemoveAccount(account);
+        }
+
         public override void CreateDepositAccount(Client client, float amountOfMoney)
         {
             var account = new DepositAccount(
-                AccountIds += 1,
+                $"{Id}" + "_" + $"{AccountIds += 1}",
                 AccountsTerm,
                 PercentCalculator.CalculateDepositPercent(amountOfMoney),
                 amountOfMoney,
                 GetClientDoubtfulness(client),
                 LimitIfDoubtful);
             Accounts.Add(account);
+            client.AddAccount(account);
+        }
+
+        public override void CloseDepositAccount(Account account)
+        {
+            CreateDebitAccount(GetClientByAccountId(account.GetId()), account.GetAmountOfMoney());
+            RemoveAccount(account);
         }
 
         public override void CreateDebitAccount(Client client, float amountOfMoney)
         {
             var account = new DebitAccount(
-                AccountIds += 1,
+                $"{Id}" + "_" + $"{AccountIds += 1}",
                 AccountsTerm,
                 PercentCalculator.CalculateDebitPercent(amountOfMoney),
                 amountOfMoney,
                 GetClientDoubtfulness(client),
                 LimitIfDoubtful);
             Accounts.Add(account);
+            client.AddAccount(account);
         }
 
         public override void CreateCreditAccount(Client client, float amountOfMoney)
         {
             var account = new CreditAccount(
-                AccountIds += 1,
+                $"{Id}" + "_" + $"{AccountIds += 1}",
                 AccountsTerm,
                 PercentCalculator.CalculateCreditCommission(amountOfMoney),
                 amountOfMoney,
                 GetClientDoubtfulness(client),
                 LimitIfDoubtful);
             Accounts.Add(account);
+            client.AddAccount(account);
         }
 
         public override void AddClient(Client client)
@@ -58,9 +78,14 @@ namespace Banks.BanksStructure.Implementations
             Clients.Add(client);
         }
 
+        public override Client GetClientByAccountId(string accountId)
+        {
+            return Clients.FirstOrDefault(client => client.GetAccountIds().Contains(accountId));
+        }
+
         public override bool GetClientDoubtfulness(Client client)
         {
-            return client.GetAddress() == null || client.GetPassportNumber() == 0;
+            return client.GetAddress() == null || client.GetPassportNumber() == null;
         }
 
         public override bool CheckIfMonthPassed(Account account)
@@ -73,6 +98,7 @@ namespace Banks.BanksStructure.Implementations
             foreach (Account account in Accounts)
             {
                 account.AddDailyIncome();
+                ReduceDaysLeft();
                 if (account is SavingsAccount savingsAccount && CheckIfMonthPassed(account))
                 {
                     savingsAccount.AddMonthlyIncome();
@@ -86,6 +112,21 @@ namespace Banks.BanksStructure.Implementations
             {
                 account.ReduceDaysLeft();
             }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Bank bank && Equals(bank);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        private bool Equals(Bank other)
+        {
+            return Id == other.Id;
         }
     }
 }
