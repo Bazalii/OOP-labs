8e1e16a5d28a0391b73c63c8268e07abbaf8df28
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-11-30 00:39:16 +0300

    feat: realise InputTransformer class
    - This class is for transformation of console input to objects for application

---
+++ Banks/ConsoleInterfaceStructure/InputTransformer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Banks.BanksStructure.Implementations;
+
+namespace Banks.ConsoleInterfaceStructure
+{
+    public class InputTransformer
+    {
+        public Client CreateClient(string name, string surname, string address, string passportNumber)
+        {
+            ClientBuilder clientBuilder = new ();
+            clientBuilder.SetName(name);
+            clientBuilder.SetSurname(surname);
+            if (address != string.Empty)
+            {
+                clientBuilder.SetAddress(address);
+            }
+
+            if (passportNumber != string.Empty)
+            {
+                clientBuilder.SetPassportNumber(passportNumber);
+            }
+
+            return clientBuilder.GetResult();
+        }
+    }
+}
