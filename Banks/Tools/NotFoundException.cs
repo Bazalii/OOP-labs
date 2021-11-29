7880e9d19cc17733c4707a37f6223135cdc2659a
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-11-29 19:10:30 +0300

    feat: add new exception

---
+++ Banks/Tools/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Banks.Tools
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message)
+            : base(message)
+        {
+        }
+    }
+}
