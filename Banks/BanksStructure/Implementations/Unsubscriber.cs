a4f64627872b213160c0dc6ebf02227163472374
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-12-01 22:29:03 +0300

    feat: add Unsubscriber class for cancellation of subscription

---
+++ Banks/BanksStructure/Implementations/Unsubscriber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Banks.BanksStructure.Implementations
+{
+    public class Unsubscriber : IMyDisposable
+    {
+        private string _name;
+
+        private List<IMyObserver> _observers;
+
+        private IMyObserver _observer;
+
+        public Unsubscriber(string name, List<IMyObserver> observers, IMyObserver observer)
+        {
+            _name = name;
+            _observers = observers;
+            _observer = observer;
+        }
+
+        public void Dispose()
+        {
+            if (_observers.Contains(_observer))
+            {
+                _observers.Remove(_observer);
+            }
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+}
