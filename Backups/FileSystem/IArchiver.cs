69d1cc3c3d198e976bdfe3395f47b16e7074a4f5
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-12-10 23:55:45 +0300

    feat: add IArchiver interface and one implementation of it
    - Realise WinZipArchiver: archiver for Win FS, which uses ZipFile

---
+++ Backups/FileSystem/IArchiver.cs
@@ -0,0 +1,9 @@
+﻿namespace Backups.FileSystem
+{
+    public interface IArchiver
+    {
+        void AddToArchive(string directoryToArchivePath, string archivePath);
+
+        void ExtractFromArchive(string archivePath, string directoryToExtract);
+    }
+}
---
+++ Backups/FileSystem/Implementations/WinZipArchiver.cs
@@ -0,0 +1,17 @@
+﻿using System.IO.Compression;
+
+namespace Backups.FileSystem.Implementations
+{
+    public class WinZipArchiver : IArchiver
+    {
+        public void AddToArchive(string directoryToArchivePath, string archivePath)
+        {
+            ZipFile.CreateFromDirectory(directoryToArchivePath, archivePath);
+        }
+
+        public void ExtractFromArchive(string archivePath, string directoryToExtract)
+        {
+            ZipFile.ExtractToDirectory(archivePath, directoryToExtract);
+        }
+    }
+}
