0587510e7deba21779a7f497fba651fc9060934f
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-12-28 00:08:27 +0300

    feat: realize ComplexStrategy class
    - This class handles all necessary algorithms for ModifiedBackupJob

---
+++ BackupsExtra/BackupExtraStructure/ComplexStrategy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Backups.Algorithms;
+using BackupsExtra.Algorithms.MergeAlgorithms;
+using BackupsExtra.Algorithms.PointRemovalAlgorithms;
+using BackupsExtra.Algorithms.RestoreAlgorithms;
+
+namespace BackupsExtra.BackupExtraStructure
+{
+    public class ComplexStrategy
+    {
+        public ComplexStrategy(
+            SavingAlgorithm savingAlgorithm,
+            RestoreAlgorithm restoreAlgorithm,
+            PointRemovalAlgorithm removalAlgorithm,
+            MergeAlgorithm mergeAlgorithm)
+        {
+            SavingAlgorithm = savingAlgorithm ??
+                              throw new ArgumentNullException(
+                                  nameof(savingAlgorithm), "SavingAlgorithm cannot be null!");
+            RestoreAlgorithm = restoreAlgorithm ??
+                               throw new ArgumentNullException(
+                                   nameof(restoreAlgorithm), "RestoreAlgorithm cannot be null!");
+            RemovalAlgorithm = removalAlgorithm ??
+                               throw new ArgumentNullException(
+                                   nameof(removalAlgorithm), "PointRemovalAlgorithm cannot be null!");
+            MergeAlgorithm = mergeAlgorithm ??
+                             throw new ArgumentNullException(
+                                 nameof(mergeAlgorithm), "MergeAlgorithm cannot be null!");
+        }
+
+        public SavingAlgorithm SavingAlgorithm { get; }
+
+        public RestoreAlgorithm RestoreAlgorithm { get; }
+
+        public PointRemovalAlgorithm RemovalAlgorithm { get; }
+
+        public MergeAlgorithm MergeAlgorithm { get; }
+    }
+}
