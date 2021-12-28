d797600c000f546c9e417f4f587e3757a80a39f6
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-12-28 23:45:38 +0300

    feat: improve RestorePoint.cs
    - Add new property Name
    - Add [JsonProperty] attributes for serialization

--- Backups/BackupStructure/RestorePoint.cs
+++ Backups/BackupStructure/RestorePoint.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Backups.BackupStructure
 {
     public class RestorePoint
     {
+        [JsonProperty]
         private readonly List<JobObject> _backupedFiles = new ();
 
-        public RestorePoint(DateTime timeOfBackup, IEnumerable<JobObject> backupedFiles)
+        public RestorePoint(string name, DateTime timeOfBackup, IEnumerable<JobObject> backupedFiles)
         {
+            Name = name ??
+                   throw new ArgumentNullException(
+                       nameof(name), "Name cannot be null!");
             TimeOfBackup = timeOfBackup;
             _backupedFiles.AddRange(backupedFiles);
         }
 
         public IReadOnlyList<JobObject> BackupedFiles => _backupedFiles;
+
         public DateTime TimeOfBackup { get; }
+
+        public string Name { get; }
     }
 }
