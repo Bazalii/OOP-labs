cd8f10d9cf60b4019e6eaf9aa372afd0b2847657
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-12-29 14:33:14 +0300

    fix: correct bug in BackupJob
    - RestorePointsCounter shows the number of all made restore points but expected return is the number of restore points that are currently in the list

--- Backups/BackupStructure/BackupJob.cs
+++ Backups/BackupStructure/BackupJob.cs
@@ -60,7 +60,7 @@ namespace Backups.BackupStructure
 
         public int GetRestorePointsNumber()
         {
-            return RestorePointsCounter;
+            return RestorePoints.Count;
         }
     }
 }
