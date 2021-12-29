39cb67ed341e7f81083ef3df06878cddff9b0cf3
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-12-29 17:28:53 +0300

    feat: realise algorithms for removing restoring points
    - Assign abstract class PointRemovalAlgorithm
    - Realise two concrete algorithms

---
+++ BackupsExtra/Algorithms/PointRemovalAlgorithms/Implementations/SingleStorageRemovalAlgorithm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Backups.BackupStructure;
+using Backups.FileSystem;
+
+namespace BackupsExtra.Algorithms.PointRemovalAlgorithms.Implementations
+{
+    public class SingleStorageRemovalAlgorithm : PointRemovalAlgorithm
+    {
+        public SingleStorageRemovalAlgorithm(IFileSystem fileSystem, string backupsDirectory)
+        {
+            FileSystem = fileSystem ??
+                         throw new ArgumentNullException(
+                             nameof(fileSystem), "FileSystem cannot be null!");
+            BackupsDirectory = backupsDirectory ??
+                               throw new ArgumentNullException(
+                                   nameof(backupsDirectory), "Backup directory cannot be null!");
+        }
+
+        public override void RemoveRestorePoints(List<RestorePoint> restorePoints)
+        {
+            foreach (RestorePoint restorePoint in restorePoints)
+            {
+                FileSystem.RemoveFile($"{BackupsDirectory}\\{restorePoint.Name}");
+            }
+        }
+    }
+}
---
+++ BackupsExtra/Algorithms/PointRemovalAlgorithms/Implementations/SplitStorageRemovalAlgorithm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Backups.BackupStructure;
+using Backups.FileSystem;
+
+namespace BackupsExtra.Algorithms.PointRemovalAlgorithms.Implementations
+{
+    public class SplitStorageRemovalAlgorithm : PointRemovalAlgorithm
+    {
+        public SplitStorageRemovalAlgorithm(IFileSystem fileSystem, string backupsDirectory)
+        {
+            FileSystem = fileSystem ??
+                         throw new ArgumentNullException(
+                             nameof(fileSystem), "FileSystem cannot be null!");
+            BackupsDirectory = backupsDirectory ??
+                               throw new ArgumentNullException(
+                                   nameof(backupsDirectory), "Backup directory cannot be null!");
+        }
+
+        public override void RemoveRestorePoints(List<RestorePoint> restorePoints)
+        {
+            foreach (RestorePoint restorePoint in restorePoints)
+            {
+                foreach (JobObject backupedFile in restorePoint.BackupedFiles)
+                {
+                    FileSystem.RemoveFile($"{BackupsDirectory}\\{restorePoint.Name}{FileSystem.GetNameFromPath(backupedFile.PathToFile)}");
+                }
+            }
+        }
+    }
+}
