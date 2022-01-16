aadc403ec4cb199d0bc5469897e7a178e0c9ff9c
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-01-16 23:27:27 +0300

    feat: realise ReportService

---
+++ Reports.Server/Services/Implementations/ReportService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Reports.DAL.Entities;
+
+namespace Reports.Server.Services.Implementations
+{
+    public class ReportService : IReportService
+    {
+        private List<Report> _reports = new ();
+
+        public Report Create(Guid authorId, string commentary)
+        {
+            Report report = new Report(Guid.NewGuid(), authorId, commentary, ReportStatus.Draft, DateTime.Now);
+            _reports.Add(report);
+            return report;
+        }
+
+        public Report FindById(Guid id)
+        {
+            return _reports.FirstOrDefault(report => report.Id == id);
+        }
+
+        public List<Guid> GetAllFinishedDailyReports()
+        {
+            List<Guid> employees = new ();
+            foreach (Report report in _reports)
+            {
+                if (DateTime.Now.Subtract(report.TimeOfCreation) < TimeSpan.FromDays(1))
+                {
+                    employees.Add(report.AuthorId);
+                }
+            }
+
+            return employees;
+        }
+    }
+}
