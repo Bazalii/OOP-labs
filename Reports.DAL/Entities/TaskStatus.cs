6dd7b296e12e0830a78b5f848399045720aabda0
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-01-17 00:01:57 +0300

    feat: realise enum for task status

---
+++ Reports.DAL/Entities/TaskStatus.cs
@@ -0,0 +1,23 @@
+﻿namespace Reports.DAL.Entities
+{
+    /// <summary>
+    /// Represents task statuses.
+    /// </summary>
+    public enum TaskStatus
+    {
+        /// <summary>
+        /// Represents open task.
+        /// </summary>
+        Open,
+
+        /// <summary>
+        /// Represents task with some changes.
+        /// </summary>
+        Active,
+
+        /// <summary>
+        /// Represents done task.
+        /// </summary>
+        Resolved,
+    }
+}
