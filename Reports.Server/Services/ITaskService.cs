b450c88efbb9b2ca17bbb1dc78c57fa9aa7468be
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-01-17 00:43:30 +0300

    feat: add interface for TaskService

---
+++ Reports.Server/Services/ITaskService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Reports.DAL.Entities;
+
+namespace Reports.Server.Services
+{
+    public interface ITaskService
+    {
+        Task Create(string description);
+
+        List<Task> GetAll();
+
+        Task FindById(Guid id);
+
+        Task GetByLatestTime();
+
+        public List<Task> GetByEmployeeChanges(Guid id);
+
+        void ChangeStatus(Guid id, TaskStatus status);
+
+        List<Task> GetTasksForTheWeek();
+    }
+}
