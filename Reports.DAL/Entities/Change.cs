05bae973890111838b1f577cd8a52827b5c3cb37
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-01-17 00:43:06 +0300

    feat: add Change class for changes in tasks

---
+++ Reports.DAL/Entities/Change.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Reports.DAL.Entities
+{
+    public class Change
+    {
+        public Change(Guid employeeId, DateTime time, string commentary)
+        {
+            EmployeeId = employeeId;
+            Time = time;
+            Commentary = commentary;
+        }
+
+        public Guid EmployeeId { get; }
+
+        public DateTime Time { get; }
+
+        public string Commentary { get; }
+    }
+}
