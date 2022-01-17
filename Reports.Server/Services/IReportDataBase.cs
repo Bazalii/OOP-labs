7c42be7a4c449513da3290aa9cdba2a440230b2c
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-01-17 17:42:24 +0300

    feat: realise DataBase emulation for ReportService
    - Add interface IReportDataBase
    - Realise ReportDataBase class

---
+++ Reports.Server/Services/IReportDataBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Reports.DAL.Entities;
+
+namespace Reports.Server.Services
+{
+    public interface IReportDataBase : IDataBase
+    {
+        public void Add(Report report);
+
+        public Report FindById(Guid id);
+
+        public List<Guid> GetAllFinishedDailyReports();
+
+        public Report Delete(Guid id);
+    }
+}
