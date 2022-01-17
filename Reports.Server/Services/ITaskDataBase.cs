a51ee1d1cc6f128f7e0c1d769a80ad7d08e99cb0
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-01-17 17:42:44 +0300

    feat: realise DataBase emulation for TaskService
    - Add interface ITaskDataBase
    - Realise TaskDataBase class

---
+++ Reports.Server/Services/ITaskDataBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Reports.DAL.Entities;
+
+namespace Reports.Server.Services
+{
+    public interface ITaskDataBase : IDataBase
+    {
+        public void Add(Task task);
+
+        public List<Task> GetAll();
+
+        public Task FindById(Guid id);
+
+        public Task GetByLatestTime();
+
+        public List<Task> GetByEmployeeChanges(Guid id);
+
+        public void ChangeStatus(Guid id, TaskStatus status);
+
+        public List<Task> GetTasksForTheWeek();
+    }
+}
